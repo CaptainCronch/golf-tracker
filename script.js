75b662cd09d9d01533336dbe6badfdaa6bd6e111
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2023-06-12 10:16:15 -0600

    all JSON data loaded with tee choices

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(courseData)
 
 function onSubmit(){
     let tee = 0;
-    $("#tee-form").children().each((index, element) => {
+    $("#tee-form").children("input").each((index, element) => {
         if( $(element).is(':checked') ){
             tee = index
         }
@@ -17,13 +17,25 @@ function onSubmit(){
     let par = $("#par")
     let handicap = $("#handicap")
 
+    let totalYards = 0;
+    let totalPar = 0;
+    let totalHandicap = 0;
+
     for (index in [...Array(18).keys()]){
         let holeData = courseData.holes[index].teeBoxes[tee]
 
         $(yardage).append(`<td>${holeData.yards}</td>`)
         $(par).append(`<td>${holeData.par}</td>`)
         $(handicap).append(`<td>${holeData.hcp}</td>`)
+
+        totalYards += holeData.yards
+        totalPar += holeData.par
+        totalHandicap += holeData.hcp
     }
+
+    $(yardage).append(`<td>${totalYards}</td>`)
+    $(par).append(`<td>${totalPar}</td>`)
+    $(handicap).append(`<td>${totalHandicap}</td>`)
 
     $("#tee-form").toggleClass("hidden")
     $("table").toggleClass("hidden")
